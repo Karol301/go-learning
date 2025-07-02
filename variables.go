e85be95652b4ba453d8cde1b2e7898244f0a3346
Karol Arabasz <karolarabasz6@gmail.com>
2025-07-03 00:02:48 +0200

    variables go

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1.6 //var x float = 1
+	y := int(x)
+	fmt.Println(x, y)
+
+	if x < 2 {
+		fmt.Println("x is less than 2")
+	} else if x > 10 {
+		fmt.Println("x is greater than 10")
+	} else {
+		fmt.Println("x is between 2 and 10")
+	}
+}
